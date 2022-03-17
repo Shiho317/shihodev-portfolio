f6f9cf06ce2cd13d4dc8ab208bd77a318052b0f8
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-03-17 07:26:10 -0700

    added hover animation

--- src/Components/Header/Header.style.ts
+++ src/Components/Header/Header.style.ts
@@ -10,7 +10,8 @@ justify-content: space-between;
 align-items: center;
 
 .logo-wrapper{
-
+  flex: 1;
+  cursor: pointer;
 }
 
 .logo-wrapper img{
@@ -18,7 +19,7 @@ align-items: center;
 }
 
 .header-nav{
-
+  flex: 1;
 }
 
 .header-nav ul {
@@ -29,16 +30,48 @@ align-items: center;
 .header-nav li{
   list-style: none;
   font-size: 1.3rem;
+  transition: all 0.3s ease;
+}
+
+.header-nav li:hover{
+  transform: translateY(-4px);
 }
 
 .header-nav a{
   color: #f5eedc;
   text-decoration: none;
   cursor: pointer;
-  transition: color 0.5s ease;
+  transition: color 0.3s ease;
 }
 
-.header-nav li a:hover{
+.header-nav a:hover{
   color: #dd4a48;
 }
+
+.media-icons{
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  gap: 2rem;
+}
+
+.icons-wrap{
+  width: 2.3rem;
+  height: 2.3rem;
+  border: 2px solid #f5eedc;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #f5eedc;
+  font-size: 1.3rem;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+.icons-wrap:hover{
+  transform: translateY(-4px);
+  background-color: #c0d8c0;
+}
+
 `
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { Wrapper } from "./Header.style";
 import LogoSvg from "./Logo.svg";
+import { SiDiscord } from 'react-icons/si';
+import { FiLinkedin, FiGithub, FiMail } from 'react-icons/fi';
 
 const Header = () => {
   return(
@@ -31,6 +33,20 @@ const Header = () => {
             </Link>
           </li>
         </ul>
+      </div>
+      <div className="media-icons">
+        <div className="icons-wrap">
+          <FiLinkedin/>
+        </div>
+        <div className="icons-wrap">
+          <SiDiscord/>
+        </div>
+        <div className="icons-wrap">
+          <FiGithub/>
+        </div>
+        <div className="icons-wrap">
+          <FiMail/>
+        </div>
       </div>
     </Wrapper>
   )
